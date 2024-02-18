f361e14d1005745fca54bfbf9f71f77afc644357
Mthabs <mthabs1@gmail.com>
2024-02-18 19:51:35 +0000

    Incorporate 'Posts' detail views, allowing users to retrieve, update, and delete posts.

--- posts/views.py
+++ posts/views.py
@@ -1,3 +1,34 @@
-from django.shortcuts import render
+from django.db.models import Count
+from rest_framework import generics, permissions, filters
+from friends_chats.permissions import IsOwnerOrReadOnly
+from .models import Post
+from .serializers import PostSerializer
+from rest_framework.parsers import MultiPartParser, FormParser
 
-# Create your views here.
+class PostListView(generics.ListCreateAPIView):
+    permission_classes = [permissions.IsAuthenticated, IsOwnerOrReadOnly]
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+    parser_classes = [MultiPartParser, FormParser]
+
+    filter_backends = [
+        filters.OrderingFilter,
+        filters.SearchFilter,
+    ]
+    search_fields = [
+        'owner__username',
+        'header',
+    ]
+    ordering_fields = [
+        'likes_count',
+        'comments_count',
+        'likes__created_at',
+    ]
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user, post_picture=self.request.data.get('post_picture'))
+
+class PostDetailView(generics.RetrieveUpdateDestroyAPIView):
+    permission_classes = [IsOwnerOrReadOnly]
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
