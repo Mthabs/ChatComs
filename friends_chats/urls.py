98e5972d5af786ed7e8558ec70b1fd56be77b18b
Mthabs <mthabs1@gmail.com>
2024-02-18 23:49:42 +0000

    add logout route in the api view

--- friends_chats/urls.py
+++ friends_chats/urls.py
@@ -14,13 +14,36 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
+from rest_framework.authtoken.views import obtain_auth_token
+from .views import root_route, logout_route
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Your API",
+        default_version='v1',
+        description="Your API description",
+        terms_of_service="https://www.yourapp.com/terms/",
+        contact=openapi.Contact(email="contact@friends.com"),
+        license=openapi.License(name="Your License"),
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+)
 
 urlpatterns = [
+    path('', root_route),
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls')),
+    # our logout route has to be above the default one to be matched first
+    path('dj-rest-auth/logout/', logout_route),
     path('dj-rest-auth/', include('dj_rest_auth.urls')),
-    path('dj-rest-auth/registration/', include('dj_rest_auth.registration.urls')),
+    path(
+        'dj-rest-auth/registration/', include('dj_rest_auth.registration.urls')
+    ),
+
     path('', include('profiles.urls')),
     path('', include('posts.urls')),
     path('', include('likes.urls')),
@@ -33,5 +56,10 @@ urlpatterns = [
     path('', include('videos.urls')),
     path('', include('likevideos.urls')),
     path('', include('videocomments.urls')),
+    path('api-token-auth/', obtain_auth_token, name='api_token_auth'),
+    #api endpoints
+    path('api/', include('friends.urls')),  # Replace 'your_app' with the actual name of your app
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 
 ]
