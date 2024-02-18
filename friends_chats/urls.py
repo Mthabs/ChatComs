b933be7fedff2707b1a928d1e877d64b5582f121
Mthabs <mthabs1@gmail.com>
2024-02-18 22:22:05 +0000

    Set up the 'PhotoComments' URL configuration file, mapping URLs accordingly.

--- friends_chats/urls.py
+++ friends_chats/urls.py
@@ -29,4 +29,5 @@ urlpatterns = [
     path('', include('friends.urls')),
     path('', include('photos.urls')),
     path('', include('likephotos.urls')),
+    path('', include('photocomments.urls')),
 ]
