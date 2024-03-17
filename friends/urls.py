3df046a92bf5b8d09facb1929eeabbcb7acfac1f
Thabani Nhleko <mthabs1@gmail.com>
2024-03-17 12:55:27 +0530

    refactor: removed previous urls

--- friends/urls.py
+++ friends/urls.py
@@ -2,11 +2,9 @@ from django.urls import path
 from friends.views import ManageFollowerView, ManageFollowingView
 
 urlpatterns = [
-    # path('friends/', FriendListCreateView.as_view(), name='friend-list-create'),
-    # path('friends/<int:pk>/', FriendUnfriendView.as_view(), name='friend-Unfriend'),
-    path('follower/', ManageFollowerView.as_view(), name='fetch_followers list'),
-    path('follower/<int:pk>/', ManageFollowerView.as_view(), name='manage_follower'),
-    path('following/', ManageFollowingView.as_view(), name='fetch_following_list'),
-    path('following/remove/<int:pk>/', ManageFollowingView.as_view(), name='remove_following')
-    path('following/add/<int:pk>/')
+    path('follower/', ManageFollowerView.as_view(), name='fetch-followers-list'),
+    path('follower/<int:pk>/', ManageFollowerView.as_view(), name='manage-follower'),
+    path('following/', ManageFollowingView.as_view(), name='fetch-following-list'),
+    path('following/remove/<int:pk>/', ManageFollowingView.as_view(), name='remove-following'),
+    path('following/add/<int:pk>/', ManageFollowingView.as_view(), name="add-following"),
 ]
