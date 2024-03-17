8892077edd9a4b050308974ec184641408c55503
Thabani Nhleko <mthabs1@gmail.com>
2024-03-17 12:14:26 +0530

    refactor: updated urls

--- friends/urls.py
+++ friends/urls.py
@@ -1,7 +1,12 @@
 from django.urls import path
-from .views import FriendListCreateView, FriendUnfriendView
+from friends.views import ManageFollowerView, ManageFollowingView
 
 urlpatterns = [
-    path('friends/', FriendListCreateView.as_view(), name='friend-list-create'),
-    path('friends/<int:pk>/', FriendUnfriendView.as_view(), name='friend-Unfriend'),
+    # path('friends/', FriendListCreateView.as_view(), name='friend-list-create'),
+    # path('friends/<int:pk>/', FriendUnfriendView.as_view(), name='friend-Unfriend'),
+    path('follower/', ManageFollowerView.as_view(), name='fetch_followers list'),
+    path('follower/<int:pk>/', ManageFollowerView.as_view(), name='manage_follower'),
+    path('following/', ManageFollowingView.as_view(), name='fetch_following_list'),
+    path('following/remove/<int:pk>/', ManageFollowingView.as_view(), name='remove_following')
+    path('following/add/<int:pk>/')
 ]
