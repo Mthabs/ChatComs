dce49d92c508590b3378e384a9ffce5c3f64224a
Thabani Nhleko <mthabs1@gmail.com>
2024-03-17 22:32:59 +0530

    fix: removed unrequired import

--- profiles/models.py
+++ profiles/models.py
@@ -1,7 +1,6 @@
-from django.db.models import Count
 from django.db import models
 from django.contrib.auth.models import User
-from followers.models import Follower
+
 
 class UserProfile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='userprofile')
