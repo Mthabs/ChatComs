77408b41dac7d72d8d6b88bf114abd9c48021b22
Thabani Nhleko <mthabs1@gmail.com>
2024-03-17 19:09:16 +0530

    refactor: modified auth view

--- friends_chats/serializers.py
+++ friends_chats/serializers.py
@@ -1,8 +1,20 @@
+from dj_rest_auth.registration.serializers import RegisterSerializer
 from dj_rest_auth.serializers import UserDetailsSerializer
+from allauth.account.adapter import get_adapter
 from rest_framework import serializers
+
 class CurrentUserSerializer(UserDetailsSerializer):
     profile_id = serializers.ReadOnlyField(source='profile.id')
     profile_image = serializers.ReadOnlyField(source='profile.image.url')
     class Meta(UserDetailsSerializer.Meta):
         fields = UserDetailsSerializer.Meta.fields + ('profile_id', 'profile_image')
+
+
+class CustomRegisterSerializer(RegisterSerializer):
+    def custom_signup(self, request, user):
+        # Skip email verification by setting email address as verified
+        user.is_active = True
+        user.save()
+
+
 
--- friends_chats/views.py
+++ friends_chats/views.py
@@ -1,38 +1,54 @@
+
+from dj_rest_auth.registration.views import RegisterView
+from .serializers import CustomRegisterSerializer
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
-from .settings import (
-    JWT_AUTH_COOKIE, JWT_AUTH_REFRESH_COOKIE, JWT_AUTH_SAMESITE,
-    JWT_AUTH_SECURE,
-)
+from rest_framework import status
+from rest_framework.response import Response
+from dj_rest_auth.views import LoginView
+from rest_framework.authtoken.models import Token
 
 
 @api_view()
 def root_route(request):
     return Response({
-        "message": "Welcome to my drf API!"
+        "message": "Backend Deployment is successful"
     })
 
 
-# dj-rest-auth logout view fix
-@api_view(['POST'])
-def logout_route(request):
-    response = Response()
-    response.set_cookie(
-        key=JWT_AUTH_COOKIE,
-        value='',
-        httponly=True,
-        expires='Thu, 01 Jan 1970 00:00:00 GMT',
-        max_age=0,
-        samesite=JWT_AUTH_SAMESITE,
-        secure=JWT_AUTH_SECURE,
-    )
-    response.set_cookie(
-        key=JWT_AUTH_REFRESH_COOKIE,
-        value='',
-        httponly=True,
-        expires='Thu, 01 Jan 1970 00:00:00 GMT',
-        max_age=0,
-        samesite=JWT_AUTH_SAMESITE,
-        secure=JWT_AUTH_SECURE,
-    )
-    return response
+class CustomRegisterView(RegisterView):
+    """
+    Overrided Registeration View to by pass Email Verification
+    """
+    serializer_class = CustomRegisterSerializer
+
+
+    def create(self, request, *args, **kwargs):
+        serializer = self.serializer_class(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        self.perform_create(serializer)
+
+        return Response(
+            {"message": "Account Created successfully"},
+            status=status.HTTP_201_CREATED,
+        )
+    
+    def perform_create(self, serializer):
+        user = serializer.save(self.request)
+        return user
+    
+
+class CustomLoginView(LoginView):
+    def post(self, request, *args, **kwargs):
+        self.serializer = self.get_serializer(data=request.data,
+                                               context={'request': request})
+        if self.serializer.is_valid():
+            # Obtain the authenticated user
+            user = self.serializer.validated_data['user']
+            # Check if the user has an existing token
+            token, created = Token.objects.get_or_create(user=user)
+            # Optionally, you may want to check the created flag to handle cases
+            # where the token already exists for the user
+            return Response({'token': token.key})
+        else:
+            return Response(self.serializer.errors, status=status.HTTP_400_BAD_REQUEST)
