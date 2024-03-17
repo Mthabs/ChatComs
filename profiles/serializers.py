5076b426d186b6c87be6e6758709f8b67b49b314
Thabani Nhleko <mthabs1@gmail.com>
2024-03-17 20:42:45 +0530

    fix: minor fixes

--- profiles/serializers.py
+++ profiles/serializers.py
@@ -1,9 +1,10 @@
 from rest_framework import serializers
+from django.shortcuts import get_object_or_404
 from profiles.models import UserProfile
 from friends.models import UserFollowing
 
 
-class CreateUserProfileSerialzier(serializers.ModelSerializer):
+class CreateUserProfileSerializer(serializers.ModelSerializer):
     """
     ModelSerializer instance to Create Userprofile details
     """
@@ -15,37 +16,43 @@ class UserProfileSerializer(serializers.ModelSerializer):
     """
     ModelSerializer instance to Fetch Userprofile details
     """
-    username = serializers.ReadOnlyField(source='user.username')
-    profile_picture = serializers.SerializerMethodField()
-    cover_photo = serializers.SerializerMethodField()
-    posts_count = serializers.SerializerMethodField()
+    username = serializers.SerializerMethodField()
+    profile_picture = serializers.FileField()
+    cover_photo = serializers.FileField()
+    # posts_count = serializers.SerializerMethodField()
     followers_count = serializers.SerializerMethodField()
     following_count = serializers.SerializerMethodField()
 
     class Meta:
         model = UserProfile
-        fields = ['id', 'user', 'username', 'profile_picture', 'cover_photo' 'posts_count', 'follower_count', 'following_count' ]
+        fields = ['id', 'user', 'username', 'profile_picture', 'cover_photo', 'followers_count', 'following_count' ]
 
-    def get_profile_picture(self, obj):
-        if obj.profile_picture:
-            return obj.profile_picture
-        else:
-            #Freepik free to use picture
-            return 'https://img.freepik.com/free-vector/illustration-businessman_53876-5856.jpg?w=740&t=st=1710659219~exp=1710659819~hmac=996a852ae757087525e6f08777ed883706c97f6f0ac40fcf9cf72d63d320cd67'
+    # def get_profile_picture(self, obj):
+    #     if obj.profile_picture:
+    #         return obj.profile_picture
+    #     else:
+    #         #Freepik free to use picture
+    #         return 'https://img.freepik.com/free-vector/illustration-businessman_53876-5856.jpg?w=740&t=st=1710659219~exp=1710659819~hmac=996a852ae757087525e6f08777ed883706c97f6f0ac40fcf9cf72d63d320cd67'
     
-    def get_cover_photo(self, obj):
-        if obj.cover_photo:
-            return obj.cover_photo
-        else:
-            return 'https://images.unsplash.com/photo-1505533321630-975218a5f66f?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
+    # def get_cover_photo(self, obj):
+    #     if obj.cover_photo:
+    #         return obj.cover_photo
+    #     else:
+    #         return 'https://images.unsplash.com/photo-1505533321630-975218a5f66f?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
 
     def get_followers_count(self, obj):
-        request = self.context.get('request')
-        return UserFollowing.objects.filter(user=request.user).count()
+        user = self.fetch_userprofile()
+        return UserFollowing.objects.filter(user=user).count()
 
     def get_following_count(self, obj):
-        request = self.context.get('request')
-        return UserFollowing.objects.filter(follower=request.user).count()
+        user = self.fetch_userprofile()
+        return UserFollowing.objects.filter(follower=user).count()
     
+    def get_username(self, obj):
+        return obj.fetch_username
     # def get_posts_count(self, obj):
     #     return obj.owner.posts.all().count()
+
+    def fetch_userprofile(self):
+        request = self.context.get('request')
+        return get_object_or_404(UserProfile, user=request.user)
--- profiles/views.py
+++ profiles/views.py
@@ -5,7 +5,7 @@ from rest_framework.permissions import IsAuthenticated, AllowAny
 from django.shortcuts import get_object_or_404
 
 from profiles.models import UserProfile
-from profiles.serializers import UserProfileSerializer, CreateUserProfileSerialzier
+from profiles.serializers import UserProfileSerializer, CreateUserProfileSerializer
 
 
 class UserProfileView(APIView):
@@ -17,9 +17,25 @@ class UserProfileView(APIView):
 
     def get(self, request):
         userprofile = self.fetch_userprofile(request)
-        serializer = UserProfileSerializer(userprofile)
-        return Response(serializer.data, status=status.HTTP_200_OK)
-
+        serializer = UserProfileSerializer(userprofile, context={'request': request})
+        return Response({"data":serializer.data}, status=status.HTTP_200_OK)
+    
+    def patch(self, request, *args, **kwargs):
+        try:
+            user_profile = self.fetch_userprofile(request)
+            serializer = CreateUserProfileSerializer(instance=user_profile, data=request.data, context={'request': request}, partial=True)
+            if serializer.is_valid(raise_exception=True):
+                serializer.save()
+                return Response({
+                    "message": "Profile Updated",
+                    "data" : serializer.data
+                }, status=status.HTTP_206_PARTIAL_CONTENT)
+        except Exception as exc:
+            return Response({
+                "message": "Error(s) occurred while trying to update profile",
+                "exception": str(exc).strip("\n")
+            }, status=status.HTTP_400_BAD_REQUEST)
+        
     def fetch_userprofile(self, request):
         return get_object_or_404(UserProfile, user=request.user)
     
@@ -27,15 +43,14 @@ class CreateUserProfile(APIView):
     """
     Instance of APIView to Create User Profile
     """
-    serializer_class = CreateUserProfileSerialzier
+    serializer_class = CreateUserProfileSerializer
     permission_classes = [AllowAny]
 
     def post(self, request):
         try:
             serializer = self.serializer_class(data=request.data)
-            if(serializer.is_valid(raise_exception=True)):
+            if serializer.is_valid(raise_exception=True):
                 serializer.save()
                 return Response( serializer.data, status=status.HTTP_201_CREATED )
         except Exception as exc:
             return Response ({"message": f"Error occured {str(exc).strip("\n")}"}, status=status.HTTP_400_BAD_REQUEST)
-        
