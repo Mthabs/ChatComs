cee8d7389efacf4d3ced4d2834e53f09b717d6cf
Thabani Nhleko <mthabs1@gmail.com>
2024-03-22 14:17:06 +0530

    refactor: no change

--- posts/views.py
+++ posts/views.py
@@ -9,55 +9,63 @@ from posts.models import Post, likes as Likes
 from profiles.models import UserProfile
 
 
+
 class PostView(APIView):
     """
-    APIView Instance to a
+    APIView Instance
     """
     permission_classes = [IsAuthenticated]
     serializer_class = PostGetSerializer
 
     def get(self, request, pk=None):
         if pk:
+            # Fetch Post by ID.
             post = self.fetch_post_by_id(pk)
             serializer = self.serializer_class(post)
             return Response(serializer.data, status=status.HTTP_200_OK)
         else:
+            #Fetch all users posts.
             user = self.fetch_user_profile(request)
             posts = self.fetch_user_posts(user)
             serializer = self.serializer_class(posts, many=True)
             return Response(serializer.data, status=status.HTTP_200_OK)
     
     def post(self, request):
+        #Create New Post.
         try:
             serializer = PostCreateSerializer(data=request.data, context = {"request":request})
             if serializer.is_valid(raise_exception=True):
                 serializer.save()
-                return Response({
-                    "message": "Post Created",
-                    "data": serializer.data
-                }, status=status.HTTP_201_CREATED)
+                return Response({ "message": "Post Created", "data": serializer.data }, status=status.HTTP_201_CREATED)
         except Exception as exc:
-            return Response({
-                "message": "Error occured",
-                "error": str(exc).strip("\n")
-            }, status=status.HTTP_400_BAD_REQUEST)
+            return Response({ "message": "Error occured", "error": str(exc).strip("\n") }, status=status.HTTP_400_BAD_REQUEST)
         
-    def destroy(self, request, pk):
+    def delete(self, request, pk):
+        #Delete Post Instance.
         instance = self.fetch_post_by_id(pk)
         instance.delete()
-        return Response({
-            "message": "Post is deleted"
-        }, status=status.HTTP_204_NO_CONTENT)
+        return Response({ "message": "Post is deleted" }, status=status.HTTP_204_NO_CONTENT)
+    
+    def patch(self, request, pk):
+        #Update Post Instance.
+        instance = self.fetch_post_by_id(pk)
+        try:
+            serializer = self.serializer_class(instance=instance, data=request.data, partial=True)
+            if serializer.is_valid(raise_exception=True):
+                serializer.save()
+                return Response({"message": "Post Updated Successfully"}, status=status.HTTP_206_PARTIAL_CONTENT)
+        except Exception as exc:
+            return Response({ "message": "Error occured", "error": str(exc).strip("\n")}, status=status.HTTP_400_BAD_REQUEST)
 
     def fetch_post_by_id(self, pk):
         return get_object_or_404(Post, id=pk)
 
-    
     def fetch_user_profile(self, request):
        return get_object_or_404(UserProfile, user=request.user)
 
     def fetch_user_posts(self, user):
         return get_list_or_404(Post, user=user)
+    
     
 class GetAllPosts(APIView):
     """
@@ -69,10 +77,8 @@ class GetAllPosts(APIView):
         instances = Post.objects.all()
         count = instances.count()
         serializer = self.serializer_class(instances, many=True)
-        return Response({
-            'count':count,
-            'posts':serializer.data
-        }, status=status.HTTP_200_OK)
+        return Response({ 'count':count, 'posts':serializer.data }, status=status.HTTP_200_OK)
+    
 
 class UserPosts(APIView):
     """
@@ -85,13 +91,11 @@ class UserPosts(APIView):
         instances = Post.objects.filter(user=userprofile)
         count = instances.count()
         serializer = self.serializer_class(instances, many=True)
-        return Response({
-            'count':count,
-            'posts':serializer.data
-        }, status=status.HTTP_200_OK)
+        return Response({ 'count':count, 'posts':serializer.data }, status=status.HTTP_200_OK)
     
     def fetch_user_profile(self, fk):
         return get_object_or_404(UserProfile, id=fk)
+    
     
 class PostLikeView(APIView):
     """
