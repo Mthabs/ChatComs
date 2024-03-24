30baa28db7514337a851b5c88a7a670146736df1
Thabani Nhleko <mthabs1@gmail.com>
2024-03-24 17:17:19 +0530

    fix: fixed allowed origin url

--- friends_chats/settings.py
+++ friends_chats/settings.py
@@ -53,7 +53,7 @@ CORS_ALLOW_CREDENTIALS = True
 
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
-    "https://chatcom-frontend-40488c6b8cb7.herokuapp.com/",
+    "https://chatcom-frontend-40488c6b8cb7.herokuapp.com",
 ]
 
 
