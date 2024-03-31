8826d0946723b5894f6108b43a5ace85bf7c973c
Thabani Nhleko <mthabs1@gmail.com>
2024-03-31 11:29:32 +0530

    created and update model

--- profiles/models.py
+++ profiles/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 from django.contrib.auth.models import User
 
 
@@ -11,6 +12,7 @@ class UserProfile(models.Model):
     cover_photo = models.ImageField(upload_to='image/', null=True, blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+    last_seen = models.DateTimeField(_("Last Seen"), auto_now=False, auto_now_add=True)
     
     class Meta:
         ordering = ['-created_at']
